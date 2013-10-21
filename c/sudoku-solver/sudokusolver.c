12454224c7142e020f4fb3ce5d2df005ce86b3d4
Mike Lopez <dev@scidentify.info>
2013-10-21 17:54:12 -0400

    .h

--- c/sudoku-solver/sudokusolver.c
+++ c/sudoku-solver/sudokusolver.c
@@ -1,17 +1,5 @@
 #include <stdio.h>
-
-#define MIN_VALUE = '1';
-#define MAX_VALUE = '9';
-#define EMPTY_VALUE = '.';
-#define NUM_VALUES = (MAX_VALUE-MIN_VALUE+1);
-#define GRID_SIZE = (NUM_VALUES*NUM_VALUES);
-#define MAX_CELL = (GRID_SIZE - 1);
-
-typedef char value;
-typedef int cell;
-typedef value sudokugrid[GRID_SIZE];
-
-int has_solution(sudokugrid game);
+#include "sudokugrid.h"
 
 int main() {
     // read the game from stdin or somewhere else
