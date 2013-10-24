b65acc4654a9e402d81123c62ad719dc71d43830
Mike Lopez <dev@scidentify.info>
2013-10-24 03:30:41 -0400

    bubble sort in ruby

--- ruby/bubble/bb.rb
+++ ruby/bubble/bb.rb
@@ -1,11 +1,16 @@
 # Sample demonstrating a bubble sort in Ruby
-chars = (0...50).map{ ('a'..'z').to_a[rand(26)] }.join
-
-
-for i in chars:
+# Average O(n2)
+#
+strs = (0...512).map{ ('a'..'z').to_a[rand(26)] }.join
+#strs = "dfnsdzvnxsdlzzlflsmkmpoklcuswwedwwercklffhgfshjjgdgcsaschsudhs"
+chars = strs.chars.to_a
+chars.each_with_index do |x, index|
+    j = 0
     while j < (chars.length - 1) do
         if chars[j] > chars[j+1] then
             chars[j], chars[j+1] = chars[j+1], chars[j]
         end
         j = j+1
     end
+end
+print chars
