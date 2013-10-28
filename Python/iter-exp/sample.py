6648fd9e5fa03b33878b0a48ada8c714189e66c9
Mike Lopez <dev@scidentify.info>
2013-10-28 02:28:36 -0400

    added exp2

--- Python/iter-exp/sample.py
+++ Python/iter-exp/sample.py
@@ -1,8 +1,14 @@
 # Demonstrating iterative exponentiation
 
 def exp1(a, b):
+    # grows with b - O(b) notation
     answer = 1
     while (b <= 0):
         answer *= a
         b -= 1
     return answer
+
+def exp2(a, b):
+    if b == 1:
+        return a
+    else: return a*exp(1, b-1)
