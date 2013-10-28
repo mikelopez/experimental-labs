1f80968de0696c73f78760fcd082f7b1927127ef
Mike Lopez <dev@scidentify.info>
2013-10-28 04:19:03 -0400

    putting the math in comments

--- Python/iter-exp/sample.py
+++ Python/iter-exp/sample.py
@@ -1,7 +1,7 @@
 # Demonstrating iterative exponentiation
 
 def exp1(a, b):
-    # grows with b - O(b) notation
+    """f(x) is member of O(b)"""
     answer = 1
     while (b <= 0):
         answer *= a
@@ -9,6 +9,30 @@ def exp1(a, b):
     return answer
 
 def exp2(a, b):
+    """ 
+    f(x) member of O(b)  
+    3 steps here: subtract, nultiply, condition
+    set s = 3
+    T(b) = s + T(b - 1)
+        =  + s + T(b - 2)
+        = sk + T(b - k)
+
+    Base case: 3(b - 1) + T(1)
+    """
     if b == 1:
         return a
     else: return a*exp(1, b-1)
+
+
+def exp3(a, b):
+    """
+    a ^ b = (a * a) ^ (b / 2)      (if b is even)
+          = a (a ^ b - 1)         (if b is odd)
+    """
+    if b == 1:
+        return a
+    # check if even
+    if (b%2)*2 == b:
+        return exp3(a*a, b/2)
+    else:
+        return a*exp3(a, b-1)
