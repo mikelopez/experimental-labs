95ceafccdb30d2ffa62f24bbe89c552e42046cb0
Mike Lopez <dev@scidentify.info>
2013-11-28 19:27:14 -0500

    gitignored

--- algorithms/heapsort/Python/test-maxheapify.py
+++ algorithms/heapsort/Python/test-maxheapify.py
@@ -4,8 +4,8 @@ from maxheap import *
 
 class TestHeapSort(unittest.TestCase):
     def test_heapsort(self):
-        A = [(x*randint(1,5)) for x in range(1, 1024)]
-        #A = [3, 5, 8, 7, 1, 13, 11, 16, 6]
+        #A = [(x*randint(1,5)) for x in range(1, 1024)]
+        A = [3, 5, 8, 7, 1, 13, 11, 16, 6]
         print A
         length = (len(A) - 1)
         build_heap(A, length)
